da576a53a60a74bc389664452dbce4fd6314239c
Ajay Awal <ajayawal565@gmail.com>
2019-09-28 11:43:45 +0545

    made first component

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "HelloWorld",
+    data(){
+        return {
+            message : 'Welcome to Vue'
+        }
+    }    
+}
+</script>
