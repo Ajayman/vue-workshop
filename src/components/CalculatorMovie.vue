0c16994ed7a37209d77c7e0c818194995a7e7d3a
Ajay Awal <ajayawal565@gmail.com>
2019-09-28 11:57:39 +0545

    add two number and select favourite movie

--- src/components/CalculatorMovie.vue
+++ src/components/CalculatorMovie.vue
@@ -6,14 +6,14 @@
       </div>
       <div class="content">
         <div class="form-group">
-          <input type="number" class="form-control" placeholder="First Number" />
+          <input type="number" v-model.number="num1" class="form-control" placeholder="First Number" />
         </div>
         <div class="form-group">
-          <input type="number" class="form-control" placeholder="Second Number" />
+          <input type="number" v-model.number="num2" class="form-control" placeholder="Second Number" />
         </div>
 
         <div class="form-group">
-          <p>The sum is</p>
+          <p>The sum is: {{num1 + num2}}</p> 
         </div>
       </div>
     </div>
@@ -25,14 +25,14 @@
 
       <div class="content">
         <div class="form-group">
-          <select class="form-control">
+          <select class="form-control" v-model="movie">
             <option>Doctor Strange in the Multiverse of Madness</option>
             <option>Thor: Love and Thunder</option>
             <Option>Joker</Option>
           </select>
         </div>
-        <div class="form-group">
-          <p>My most anticipated movie is:</p>
+        <div v-if="movie" class="form-group">
+          <p>My most anticipated movie is: {{movie}}</p>
         </div>
       </div>
     </div>
@@ -40,7 +40,15 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data () {
+    return {
+      num1: 0,
+      num2: 0,
+      movie: 'Joker'
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
